a8263813eac76e5fa5402b7ba7403a42a0667d80
chanbengz <chanben04gz@gmail.com>
2024-11-16 13:38:57 +0800

    chore: update grammar name to coop with ast

--- src/parser/src/lib.rs
+++ src/parser/src/lib.rs
@@ -1,7 +1,7 @@
 use lalrpop_util::lalrpop_mod;
 
 lalrpop_mod!(pub grammar); // synthesized by LALRPOP
-pub use grammar::ExprParser;
+pub use grammar::CompExprParser;
 
 
 #[cfg(test)]
@@ -14,14 +14,14 @@ mod tests {
         // Test if evaluation order is correct
         let source_code = "2 + 4 * 5";
         let lexer = spl_lexer::lexer::Lexer::new(&source_code);
-        let parser = ExprParser::new();
+        let parser = CompExprParser::new();
         let ast = parser.parse(&mut errors, lexer).unwrap();
         assert_eq!(format!("{}", ast), "(2: i32 + (4: i32 * 5: i32))");
 
         // Test expression with bracket
         let source_code = "(2 + 4) * 5";
         let lexer = spl_lexer::lexer::Lexer::new(&source_code);
-        let parser = ExprParser::new();
+        let parser = CompExprParser::new();
         let ast = parser.parse(&mut errors, lexer).unwrap();
         assert_eq!(format!("{}", ast), "((2: i32 + 4: i32) * 5: i32)");
     }
@@ -33,8 +33,8 @@ mod tests {
         // Test if evaluation order is correct
         let source_code = "2 +  * 5";
         let lexer = spl_lexer::lexer::Lexer::new(&source_code);
-        let expr = ExprParser::new().parse(&mut errors, lexer).unwrap();
-        assert_eq!(format!("{}", expr), "(2: i32 + (Error: Missing Term * 5: i32))");
+        let expr = CompExprParser::new().parse(&mut errors, lexer).unwrap();
+        assert_eq!(format!("{}", expr), "(2: i32 + (MissingTermError * 5: i32))");
     }
 }
 
