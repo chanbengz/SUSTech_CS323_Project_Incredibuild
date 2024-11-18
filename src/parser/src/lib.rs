621adbd2093c7052816ec41a75404ef290c7557e
Jaredan <Jaredanjerry@outlook.com>
2024-11-18 17:51:58 +0800

    Add: Struct definition Fix: some bugs on function TODO: Further improvement on struct referencing and obj management

--- src/parser/src/lib.rs
+++ src/parser/src/lib.rs
@@ -5,15 +5,21 @@ pub use grammar::CompExprParser;
 pub use grammar::ParaDecsParser;
 pub use grammar::FuncDecParser;
 pub use grammar::BodyParser;
+pub use grammar::StmtParser;
+pub use grammar::ProgramParser;
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs::File;
+    use std::io::Read;
 
     enum Parser {
         CompExprParser,
         ParaDecsParser,
         FuncDecParser,
+        StmtParser,
+        ProgramParser,
         #[allow(dead_code)]
         BodyParser,
     }
@@ -27,6 +33,28 @@ mod tests {
             Parser::ParaDecsParser => assert_eq!(format!("{}", ParaDecsParser::new().parse(&mut errors, lexer)
                     .unwrap().iter().map(|x| format!("{}", x)).collect::<Vec<String>>().join(", ")), expected), 
             Parser::FuncDecParser => assert_eq!(format!("{}", FuncDecParser::new().parse(&mut errors, lexer).unwrap()), expected),
+            Parser::StmtParser => assert_eq!(format!("{}", StmtParser::new().parse(&mut errors, lexer).unwrap()), expected),
+            Parser::ProgramParser => assert_eq!(format!("{}", ProgramParser::new().parse(&mut errors, lexer).unwrap()), expected),
+            Parser::BodyParser => assert_eq!(format!("{}", BodyParser::new().parse(&mut errors, lexer).unwrap()), expected),
+        }
+    }
+
+    fn assert_parse_from_file(parser: Parser, file_path: &str, expected: &str){
+        let mut file_content = String::new();
+        let mut file = File::open(file_path).expect("Unable to open file");
+        file.read_to_string(&mut file_content)
+            .expect("Unable to read file");
+    
+        let mut errors = Vec::new();
+        let lexer = spl_lexer::lexer::Lexer::new(&file_content);
+
+        match parser {
+            Parser::CompExprParser => assert_eq!(format!("{}", CompExprParser::new().parse(&mut errors, lexer).unwrap()), expected),
+            Parser::ParaDecsParser => assert_eq!(format!("{}", ParaDecsParser::new().parse(&mut errors, lexer)
+                    .unwrap().iter().map(|x| format!("{}", x)).collect::<Vec<String>>().join(", ")), expected), 
+            Parser::FuncDecParser => assert_eq!(format!("{}", FuncDecParser::new().parse(&mut errors, lexer).unwrap()), expected),
+            Parser::StmtParser => assert_eq!(format!("{}", StmtParser::new().parse(&mut errors, lexer).unwrap()), expected),
+            Parser::ProgramParser => assert_eq!(format!("{}", ProgramParser::new().parse(&mut errors, lexer).unwrap()), expected),
             Parser::BodyParser => assert_eq!(format!("{}", BodyParser::new().parse(&mut errors, lexer).unwrap()), expected),
         }
     }
@@ -41,8 +69,8 @@ mod tests {
 
     #[test]
     fn test_error_recovery() {
-        assert_parse(Parser::CompExprParser, "2 + * 5", "(2: i32 + (MissingTermError * 5: i32))");
-        assert_parse(Parser::CompExprParser, "2 + * 5 *", "(2: i32 + ((MissingTermError * 5: i32) * MissingTermError))");
+        assert_parse(Parser::CompExprParser, "2 + * 5", "(2: i32 + ([CompExprError] * 5: i32))");
+        assert_parse(Parser::CompExprParser, "2 + * 5 *", "(2: i32 + (([CompExprError] * 5: i32) * [CompExprError]))");
     }
 
     #[test]
@@ -62,18 +90,64 @@ mod tests {
     #[test]
     fn test_if() {
         // Test if statement
-        assert_parse(Parser::FuncDecParser, "
-            int main(){
-                int a = 3;
-                while (true){
-                    a = a + 1;
-                    if (a == 5){
-                        break;
-                    }
-                }
-                return;
-            }
-        ", "Function: main:[Body: [Assignment: Variable Declaration: a = [0: i32] with dimensions [] = 3: i32, While Loop (Condition: true): \n do Body: [Assignment: a = (a + 1: i32), If: Condition: a == 5: i32 then Body: [Break]], Return: null]]");
+        assert_parse(Parser::FuncDecParser, "int func(int a, int b) { if(a > b) { return a; }}", "Function: func:[Body: [If: Condition: a > b then Body: [Return: a]]]");
+        // Test if else statement
+        assert_parse(Parser::FuncDecParser, "int func(int a, int b) { if(a > b) { return a; } else { return b; }}", "Function: func:[Body: [If: Condition: a > b then Body: [Return: a] else Body: [Return: b]]]");
     }
+
+    #[test]
+    fn test_loop() {
+        // Test while loop
+        assert_parse(Parser::FuncDecParser, "int func(int a, int b) { while(a > b) { a = a - 1; }}", "Function: func:[Body: [While Loop (Condition: a > b): \n do Body: [Assignment: a = (a - 1: i32)]]]");
+        // Test for loop
+        assert_parse(Parser::FuncDecParser, "int func(int a, int b) { for(int i = 0; i < 10; i = i + 1) { a = a + 1; }}", 
+            "Function: func:[Body: [For Loop ([Initial] Assignment: Variable Declaration: i = [0: i32] with dimensions [] = 0: i32; [Condition] Condition: i < 10: i32; [Increment] Assignment: i = (i + 1: i32)): \n do Body: [Assignment: a = (a + 1: i32)]]]");
+        // Test break and continue
+        assert_parse(Parser::FuncDecParser, "int func(int a, int b) { while(a > b) { if (a == 5) { break; } continue; }}", 
+            "Function: func:[Body: [While Loop (Condition: a > b): \n do Body: [If: Condition: a == 5: i32 then Body: [Break], Continue]]]");
+    }
+
+    #[test]
+    fn test_assignexpr() {
+        // Test assignment expression
+        assert_parse(Parser::FuncDecParser, "int func(int a, int b) { int a = 2; int b = a; b = 3; b++; a--; return b + a; }", 
+        "Function: func:[Body: [Assignment: Variable Declaration: a = [0: i32] with dimensions [] = 2: i32, Assignment: Variable Declaration: b = [0: i32] with dimensions [] = a, Assignment: b = 3: i32, Assignment: b = (b + 1: i32), Assignment: a = (a - 1: i32), Return: (b + a)]]");
+    }
+
+    #[test]
+    fn test_func_call() {
+        // Test function call
+        assert_parse(Parser::FuncDecParser, "int func(int a, int b) { add(a, b); return 0; }", 
+        "Function: func:[Body: [FuncCall: add[a, b], Return: 0: i32]]");
+        assert_parse(Parser::FuncDecParser, "int func(int a, int b) { return add(a, b); }",
+        "Function: func:[Body: [Return: FuncCall: add[a, b]]]");
+        assert_parse(Parser::FuncDecParser, "int func(int a, int b) { int k = add(add(a, b), b); return k; }",
+        "Function: func:[Body: [Assignment: Variable Declaration: k = [0: i32] with dimensions [] = FuncCall: add[FuncCall: add[a, b], b], Return: k]]");
+    }
+
+    #[test]
+    fn test_stmt(){
+        assert_parse(Parser::StmtParser, "int a;", "Global Variable: Variable Declaration: a = [0: i32] with dimensions []");
+        assert_parse(Parser::StmtParser, "struct obj { int a; char b; };", "Struct obj: [Variable Declaration: a = [0: i32] with dimensions [], Variable Declaration: b = [ : char] with dimensions []]");
+        assert_parse(Parser::StmtParser, "#include \"../hi.h\";", "Include: ../hi.h");
+    }
+
+    #[test]
+    fn test_0_r00() {
+        assert_parse_from_file(Parser::FuncDecParser, "../test/test_0_r00.spl", 
+        "Function: main:[Body: [Assignment: Variable Declaration: a = [0: i32] with dimensions [] = 3: i32, While Loop (Condition: true): \n do Body: [Assignment: a = (a + 1: i32), If: Condition: a == 5: i32 then Body: [Break]], Return: null]]");
+    }
+
+    #[test]
+    fn test_1_r01() {
+        assert_parse_from_file(Parser::FuncDecParser, "../test/test_1_r01.spl", 
+        "Function: test_1_r01:[Body: [Assignment: c = c: char, If: Condition: a > b then Body: [Return: a] else Body: [Return: b]]]");
+    }
+
+    // #[test]
+    // fn test_1_r02() {
+    //     assert_parse_from_file(Parser::ProgramParser, "../test/test_1_r02.spl", 
+    //     "Function: test_1_r02:[Body: [Assignment: c = c: char, If: Condition: a > b then Body: [Return: a] else Body: [Return: b]]]");
+    // }
 }
 
