ce04ae958bbf6a5146f022fd642370fee05c8fab
chanbengz <chanben04gz@gmail.com>
2024-12-11 02:52:30 +0800

    chore: add verbose flag to analyser and update docs
    fix: main wrapper exits if earlier errors found
    
    fix: add return value of test_r00_0
    
    fix: lexer results from test_0_r00

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 use spl_parser::{parse_from_file};
 use spl_analyser::walker::Walker;
-use clap::{Arg, Command};
+use clap::{Arg, Command, ArgAction};
 use colored::Colorize;
 
-fn main() {
+fn main() -> Result<(), String> {
     let args = Command::new("Incredibuild")
         .about("Compile SPL code to executable")
-        .arg(
-            Arg::new("input").index(1).required(true)
-        )
-        .arg(
-            Arg::new("output").short('o').long("output").required(false)
-        )
+        .arg(Arg::new("input").index(1).required(true))
+        .arg(Arg::new("output").short('o').long("output").required(false))
+        .arg(Arg::new("debug").short('d').long("debug").required(false).action(ArgAction::SetTrue))
         .get_matches();
 
     let source_path = args.get_one::<String>("input").unwrap();
@@ -21,13 +18,19 @@ fn main() {
         Ok(_) => println!("{}", "Parsed successfully".green()),
         Err(e) => {
             println!("{}", e.red());
+            return Err("Error in parsing".to_string());
         }
     }
 
-    let mut walker = Walker::new(&source_path);
+    let mut walker = Walker::new(&source_path, args.get_flag("debug"));
     walker.traverse();
     let errors = walker.get_errors();
     for error in errors {
         println!("{}", error.to_string().red());
     }
+    if errors.len() > 0 {
+        return Err("Error in analysis".to_string());
+    }
+
+    Ok(())
 }
